eca8815d0c902b87796da9ec65dff8412157986f
Tahira Tariq <tariqt@rpi.edu>
2023-10-05 16:34:36 -0400

    Working on error handling middleware

--- root/backend/middleware/errorHandler.js
+++ root/backend/middleware/errorHandler.js
@@ -1,22 +1,41 @@
-const errorHandler(err, req, res, next) => {
+const { constants } = require("../constants");
+const errorHandler = (err, req, res, next) => {
     const statusCode = res.statusCode ? res.statusCode : 500;
     switch (statusCode){
-        case 400:
+        case constants.VALIDATION_ERROR:
             res.json({
-                title: "Not Found", 
+                title: "validation Failed",
                 message: err.message, 
                 stackTrace: err.stack,
             });
             break;
-        case 404:
+        case constants.NOT_FOUND:
             res.json({
-                title: "validation Failed", 
+                title: "Not Found",  
                 message: err.message, 
                 stackTrace: err.stack,
             });
-            default;
-                break;
+        case constants.UNAUTHORIZED:
+            res.json({
+                title: "Unauthorized",  
+                message: err.message, 
+                stackTrace: err.stack,
+            });
+        case constants.FORBIDDEN:
+            res.json({
+                title: "Forbidden",  
+                message: err.message, 
+                stackTrace: err.stack,
+            });
+        case constants.SERVER_ERROR:
+            res.json({
+                title: "Server Error",  
+                message: err.message, 
+                stackTrace: err.stack,
+            });
+        default:
+            console.log("No Error");
+            break;
     }
 };
-
 module.exports = errorHandler;
