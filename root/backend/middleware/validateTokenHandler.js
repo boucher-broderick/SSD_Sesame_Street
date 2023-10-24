f90eaf422d8e6ce8268a2866db34c0faef888992
Edward <edmolina423@gmail.com>
2023-10-24 19:26:02 -0400

    Added delete user function

--- root/backend/middleware/validateTokenHandler.js
+++ root/backend/middleware/validateTokenHandler.js
@@ -2,24 +2,29 @@ const asyncHandler = require("express-async-handler");
 const jwt = require("jsonwebtoken");
 
 const validateToken = asyncHandler(async (req, res, next) => {
-  let token;
-  let authHeader = req.headers.Authorization || req.headers.authorization;
-  if (authHeader && authHeader.startsWith("Bearer")) {
-    token = authHeader.split(" ")[1];
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECERT, (err, decoded) => {
-      if (err) {
+    let token;
+    let authHeader = req.headers.Authorization || req.headers.authorization;
+
+    if (authHeader && authHeader.startsWith("Bearer")) {
+        token = authHeader.split(" ")[1];
+        
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+            if (err) {
+                res.status(401);
+                throw new Error("User is not authorized");
+            }
+            req.user = decoded.user;
+            next();
+        });
+    } else {
         res.status(401);
-        throw new Error("User is not authorized");
-      }
-      req.user = decoded.user;
-      next();
-    });
+        throw new Error("Authorization token must start with Bearer");
+    }
 
     if (!token) {
-      res.status(401);
-      throw new Error("User is not authorized or token is missing");
+        res.status(401);
+        throw new Error("User is not authorized or token is missing");
     }
-  }
 });
 
 module.exports = validateToken;
