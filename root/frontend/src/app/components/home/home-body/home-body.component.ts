e6e7f32d1f8176a66d6593d2b0b2277bfef81b24
Hanson Gu <hansongu123@gmail.com>
2023-12-01 19:16:34 -0500

    homepage, signin signup frontend refactored

--- root/frontend/src/app/components/home/home-body/home-body.component.ts
+++ root/frontend/src/app/components/home/home-body/home-body.component.ts
@@ -1,15 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HomeService } from '../home.service';
+import { PhotoService } from '../photo.service';
 
 @Component({
   selector: 'app-home-body',
   templateUrl: './home-body.component.html',
   styleUrls: ['./home-body.component.css'],
 })
-export class HomeBodyComponent {
+export class HomeBodyComponent implements OnInit {
+  images: any[] | undefined;
+  responsiveOptions: any[] | undefined;
+  description = "a writer's planning tool that helps to gather and organize inspiration in the form of notes, links, songs, images, and many else.";
   public screen: number = 0;
 
-  constructor(private homeService: HomeService){}
+  constructor(private homeService: HomeService, private photoService: PhotoService){}
 
   // to see which screen should be displayed
   ngOnInit(){
@@ -17,12 +21,33 @@ export class HomeBodyComponent {
       this.screen = screen;
     }
     )
+    this.photoService.getImages().then((images) => (this.images = images));
+        this.responsiveOptions = [
+            {
+                breakpoint: '1024px',
+                numVisible: 5
+            },
+            {
+                breakpoint: '768px',
+                numVisible: 3
+            },
+            {
+                breakpoint: '560px',
+                numVisible: 1
+            }
+        ];
+  }
+  showSignUpWindow = false;
+  toggleSignUpWindow() {
+    this.showSignUpWindow = true;
   }
 
-  showSignUpWindow = false;
-  signInVisible = false;
-  toggleSignUpWindow() {
-    this.showSignUpWindow = !this.showSignUpWindow;
-    this.signInVisible = this.showSignUpWindow;
+  changeScreen(screen: number){
+    this.homeService.changeComponent(screen);
+  }
+
+  redirectToDoc() {
+    const link = 'https://github.com/boucher-broderick/SSD_Sesame_Street';
+    window.location.href = link;
   }
 }
